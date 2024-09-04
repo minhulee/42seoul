99db2f08ca3c188552b64dd51d25bc0d8f392e2b
minhulee <dlalsgns9039@gmail.com>
2024-09-04 17:05:15 +0900

    Add : circle_4/cub3d - set_start()

--- circle_4/cub3d/srcs/1_parsing/util.c
+++ circle_4/cub3d/srcs/1_parsing/util.c
@@ -6,40 +6,67 @@
 /*   By: minhulee <minhulee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 17:56:54 by minhulee          #+#    #+#             */
-/*   Updated: 2024/08/27 16:01:03 by minhulee         ###   ########seoul.kr  */
+/*   Updated: 2024/09/04 11:13:40 by minhulee         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-void	test_map_array(t_map_data *map_data, t_tile_type **map)
+static void	setup_camera(t_tile_type type, t_camera *camera)
 {
-	(void)map_data;
-	int	i = 0;
-	
-	while (i < map_data->height + 2)
+	if (type == ST_N)
 	{
-		ft_printf("%d", i);
-		// 2 | 0 -> 2개
-		for (int a = map_data->height / 10 + 1; a > i / 10; a--)
-			ft_printf(" ");
-		ft_printf("[");
-		int	j = 0;
-		while (j < map_data->width + 2)
-		{
-			if (map[i][j] == EMPTY)
-				ft_printf("  ");
-			else if (map[i][j] == GROUND)
-				ft_printf("0 ");
-			else if (map[i][j] == WALL)
-				ft_printf("1 ");
-			else if (map[i][j] == START)
-				ft_printf("N ");
-			j++;
-		}
-		ft_printf("]\n");
-		i++;
+		camera->plane_x = 0.66;
+		camera->plane_y = 0;
 	}
+	else if (type == ST_S)
+	{
+		camera->plane_x = -0.66;
+		camera->plane_y = 0;
+	}
+	else if (type == ST_E)
+	{
+		camera->plane_x = 0;
+		camera->plane_y = 0.66;
+	}
+	else if (type == ST_W)
+	{
+		camera->plane_x = 0;
+		camera->plane_y = -0.66;
+	}
+}
+
+static void	setup_player(t_tile_type type, t_player *player)
+{
+	if (type == ST_N)
+	{
+		player->dir_x = 0;
+		player->dir_y = -1;
+	}
+	else if (type == ST_S)
+	{
+		player->dir_x = 0;
+		player->dir_y = 1;
+	}
+	else if (type == ST_E)
+	{
+		player->dir_x = 1;
+		player->dir_y = 0;
+	}
+	else if (type == ST_W)
+	{
+		player->dir_x = -1;
+		player->dir_y = 0;
+	}
+}
+
+void	set_start(t_cub3d *info, int h, int w, t_tile_type type)
+{
+	info->map_data.map[h][w] = type;
+	info->player.pos_x = w;
+	info->player.pos_y = h;
+	setup_camera(type, &info->camera);
+	setup_player(type, &info->player);
 }
 
 int	quick_open_file(char *file, int line)
