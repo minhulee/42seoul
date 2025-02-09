f2a1f0b78064c51f1a2853349370888dc61a1449
minhulee <dlalsgns9039@gmail.com>
2025-02-09 17:48:10 +0900

    Add & Modify : circle_5/cpp/cpp09/ex00, ex01 (temp)

--- circle_5/cpp/cpp09/ex00/BitcoinExchange.cpp
+++ circle_5/cpp/cpp09/ex00/BitcoinExchange.cpp
@@ -6,27 +6,34 @@
 /*   By: minhulee <minhulee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 14:59:42 by minhulee          #+#    #+#             */
-/*   Updated: 2025/02/06 18:01:03 by minhulee         ###   ########seoul.kr  */
+/*   Updated: 2025/02/09 14:39:14 by minhulee         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./BitcoinExchange.hpp"
 
-#include <fstream>
 #include <iostream>
 #include <string>
+#include <fstream> // -> ifstream OBJ
+#include <sstream> // -> iss OBJ
+#include <cfloat> // -> FLT_LIMITS
+#include <cstdlib> // -> strtod()
+
+#define ERROR -1
+#define INVALID_DATE -1
+#define INVALID_FORMAT -2
+#define NOT_POSITIVE -3
+#define TOO_LARGE -4
+#define NO_DATA -5
 
 void	BitcoinExchange::run(std::string path)
 {
-	std::map<std::string, int>	map;
-	std::ifstream				input;
+	std::map<std::string, float>	mData;
 
-	(void)path;
 	try
 	{
-		loadData(map);
-		loadInput(input, path);
-		inputExchangeData(map, input);
+		loadData(mData);
+		inputExchangeData(mData, path);
 	}
 	catch (std::exception &e)
 	{
@@ -34,25 +41,80 @@ void	BitcoinExchange::run(std::string path)
 	}
 }
 
-void	BitcoinExchange::lineToMap(std::map<std::string, int> &map, const char *seperator, std::string line)
+// yyyy-mm-dd 유효성 검사
+std::string	BitcoinExchange::validDate(const std::string line)
 {
-	size_t		n = line.find(seperator);
+	if (line.length() != 10)
+		return ("");
+	
+	std::istringstream	iss(line);
+	int year, month, day;
+	char s1, s2;
+
+	iss >> year >> s1 >> month >> s2 >> day;
+
+	if (iss.fail() || s1 != '-' || s2 != '-')
+		return ("");
+
+	if (month > 12)
+		return ("");
+
+	int	days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	// 윤년 계산
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+		days[1] = 29;
+
+	if (days[month - 1] < day)
+		return ("");
+
+	return (line);
+}
+
+float	BitcoinExchange::validValue(const std::string line)
+{
+	if (line.length() == 0 || line[0] == ' ')
+		return (INVALID_FORMAT);
+	
+	double	res;
+	char	*eptr;
+
+	res = std::strtod(line.c_str(), &eptr);
+	
+	if (*eptr != '\0')
+		return (INVALID_FORMAT);
+
+	if (res < 0)
+		return (NOT_POSITIVE);
+
+	if (FLT_MAX < res)
+		return (TOO_LARGE);
+
+	return (res);
+}
+
+void	BitcoinExchange::lineToMap(std::map<std::string, float> &map, const char *seperator, std::string line)
+{
+	size_t	n = line.find(seperator);
 
 	if (n == std::string::npos)
 		throw InvalidFormat();
 	
-	std::string	key = line.substr(0, n);
-	char		*eptr;
-	double		value = std::strtod(line.substr(n + 1, line.size() - 1).c_str(), &eptr);
+	std::string	key = validDate(line.substr(0, n));
 
-	if (*eptr != '\0')
-		throw StrtodConvertFailed();
+	if (key == "")
+		throw InvalidDate();
+
+	float	value = validValue(line.substr(n + 1, line.length()));
+
+	if (value < 0)
+		throw InvalidValue();
 	
 	map[key] = value;
 }
 
 // data.csv open, 별도의 예외처리 x
-void	BitcoinExchange::loadData(std::map<std::string, int> &map)
+void	BitcoinExchange::loadData(std::map<std::string, float> &mData)
 {
 	std::ifstream	data("./data.csv");
 
@@ -68,20 +130,48 @@ void	BitcoinExchange::loadData(std::map<std::string, int> &map)
 	try
 	{
 		while (std::getline(data, line))
-			lineToMap(map, ",", line);
+			lineToMap(mData, ",", line);
 	}
 	catch (std::exception &e)
 	{
 		throw ;
 	}
+	
 	data.close();
 }
 
-// input.txt open, 별도의 예외처리 x
-void	BitcoinExchange::loadInput(std::ifstream &input, std::string path)
+float	searchValue(std::string date, std::map<std::string, float> &mData)
 {
-	input.open(path);
+	std::map<std::string, float>::iterator	itr = mData.lower_bound(date);
+	if (itr == mData.begin())
+		return (NO_DATA);
 
+	if (itr == mData.end() || itr->first != date)
+		itr--;
+
+	return (itr->second);
+}
+
+void	eprint(int mode, size_t n, std::string line)
+{
+	std::string	date = line.substr(0, n);
+	std::string value = line.substr(n + 3, line.length());
+
+	switch (mode)
+	{
+		case INVALID_DATE:		std::cerr << "Error: bad input => [" << date << "]" << std::endl;				break ;
+		case INVALID_FORMAT:	std::cerr << "Error: invlid format. => [" << value << "]" << std::endl;			break ;
+		case NOT_POSITIVE:		std::cerr << "Error: not a positive number. => [" << value << "]" << std::endl;	break ;
+		case TOO_LARGE:			std::cerr << "Error: too large number => [" << value << "]" << std::endl;		break ;
+		case NO_DATA:			std::cerr << "Error: no data. [" << date << "]" << std::endl;					break ;
+	}
+}
+
+// input * data의 결과 출력 및 유효성 검사
+void	BitcoinExchange::inputExchangeData(std::map<std::string, float> &mData, std::string path)
+{
+	std::ifstream	input(path.c_str());
+	
 	if (!input.is_open())
 		throw InputOpenFailed();
 
@@ -90,18 +180,28 @@ void	BitcoinExchange::loadInput(std::ifstream &input, std::string path)
 
 	if (line != "date | value")
 		throw InvalidFormat();
-}
 
-void	BitcoinExchange::validData(std::string line)
-{
+	while (std::getline(input, line))
+	{
+		size_t	n = line.find(" | ");
+		
+		std::string	date = validDate(line.substr(0, n));
+		float value = validValue(line.substr(n + 3, line.length()));
+		float mValue = searchValue(date, mData);
 	
-}
-
-// input * data의 결과 출력 및 유효성 검사
-void	BitcoinExchange::inputExchangeData(std::map<std::string, int> &map, std::ifstream &input)
-{
-	std::string	line;
-	
+		if (n == std::string::npos)
+			eprint(INVALID_FORMAT, n, line);
+		else if (date == "")
+			eprint(INVALID_DATE, n, line);
+		else if (value <= ERROR)	
+			eprint(value, n, line);
+		else if (value > 1000)
+			eprint(TOO_LARGE, n, line);
+		else if (mValue < ERROR)
+			eprint(mValue, n, line);
+		else
+			std::cout << date << " => " << value << " = " << value * mValue << std::endl;
+	}
 
 	input.close();
 }
@@ -121,7 +221,12 @@ const char	*BitcoinExchange::InvalidFormat::what() const throw()
 	return ("exception :: invalid format :: ");
 }
 
-const char	*BitcoinExchange::StrtodConvertFailed::what() const throw()
+const char	*BitcoinExchange::InvalidDate::what() const throw()
 {
-	return ("exception :: failed strtod() func (not integer or float) :: ");
+	return ("exception :: invalid data :: ");
+}
+
+const char	*BitcoinExchange::InvalidValue::what() const throw()
+{
+	return ("exception :: invalid value :: ");
 }
--- circle_5/cpp/cpp09/ex00/BitcoinExchange.hpp
+++ circle_5/cpp/cpp09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: minhulee <minhulee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 14:58:12 by minhulee          #+#    #+#             */
-/*   Updated: 2025/02/06 18:00:48 by minhulee         ###   ########seoul.kr  */
+/*   Updated: 2025/02/09 12:57:09 by minhulee         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,25 @@
 # define	BitcoinExchange_hpp
 
 #include <map>
+#include <string>
 #include <exception>
 
 class	BitcoinExchange
 {
 	/* user can acess only this func */
 	public:
-		static void	run(std::string);
+		static void		run(std::string);
 
 	/* encapsulates the main funcs */
 	private:
-		static void	validData(std::string line);
-		static void	lineToMap(std::map<std::string, int> &map, const char *seperator, std::string line);
-		static void	loadData(std::map<std::string, int> &map);
-		static void	loadInput(std::ifstream &, std::string);
-		static void	inputExchangeData(std::map<std::string, int> &, std::ifstream &);
+		static std::string	validDate(std::string);
+		static float	validValue(std::string);
+		static void		lineToMap(std::map<std::string, float> &, const char *, std::string);
+
+		static void		loadData(std::map<std::string, float> &);
+		// static void		loadInput(std::map<std::string, float> &, std::string);
+		
+		static void		inputExchangeData(std::map<std::string, float> &, std::string);
 
 	/* exceptions */
 	public:
@@ -50,7 +54,13 @@ class	BitcoinExchange
 				const char	*what() const throw();
 		};
 
-		class	StrtodConvertFailed : public std::exception
+		class	InvalidDate : public std::exception
+		{
+			public:
+				const char	*what() const throw();
+		};
+
+		class	InvalidValue : public std::exception
 		{
 			public:
 				const char	*what() const throw();
